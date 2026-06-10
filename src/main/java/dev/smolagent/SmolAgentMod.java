3104bf00fcabd6812377fd4df45d696612f4abc2
paraf0x <paraf0x@users.noreply.github.com>
2026-06-10 09:25:49 +0200

    feat: mod entry point + Fabric metadata + empty mixin manifest

---
+++ src/main/java/dev/smolagent/SmolAgentMod.java
@@ -0,0 +1,10 @@
+package dev.smolagent;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmolAgentMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // All behavior is in Mixins; entry point intentionally empty.
+    }
+}
